993709f255c1d1370733ff12ffe11d7925c2f304
smonrou <saulnadremm@gmail.com>
2026-04-25 14:07:15 -0600

    first commit

---
+++ src/main/java/gt/edu/cunori/expedientes/ExpedientesApplication.java
@@ -0,0 +1,13 @@
+package gt.edu.cunori.expedientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpedientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpedientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gt/edu/cunori/expedientes/ExpedientesApplicationTests.java
@@ -0,0 +1,13 @@
+package gt.edu.cunori.expedientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpedientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
